b6582ece5949293b27e7a56da7bea28e0bdd9d55
Prachi <bhatnagar.prachi845@gmail.com>
2019-01-27 12:15:54 +0000

    DailyTradeReporting

---
+++ DailyTradeReportingEngine-master/src/main/java/business_logic/working_days/ArabiaWorkingDays.java
@@ -0,0 +1,31 @@
+package business_logic.working_days;
+
+import java.time.DayOfWeek;
+
+public class ArabiaWorkingDays extends WorkingDays {
+
+    private static ArabiaWorkingDays instance = null;
+    
+//No synchronisation reqd. as its single threaded application.
+    public static ArabiaWorkingDays getInstance() {
+        if (instance == null) {
+            instance = new ArabiaWorkingDays();
+        }
+        return instance;
+    }
+
+    private ArabiaWorkingDays() {
+        super();
+    }
+
+    @Override
+    protected void setupWorkingDays() {
+        this.isWorkingDayMap.put(DayOfWeek.SUNDAY, true);
+        this.isWorkingDayMap.put(DayOfWeek.MONDAY, true);
+        this.isWorkingDayMap.put(DayOfWeek.TUESDAY, true);
+        this.isWorkingDayMap.put(DayOfWeek.WEDNESDAY, true);
+        this.isWorkingDayMap.put(DayOfWeek.THURSDAY, true);
+        this.isWorkingDayMap.put(DayOfWeek.FRIDAY, false); // in arabia those are not working days
+        this.isWorkingDayMap.put(DayOfWeek.SATURDAY, false); // in arabia those are not working days
+    }
+}
